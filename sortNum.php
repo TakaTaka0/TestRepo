bdd0d0cba3e7e3310dbab4da4f09c310d34d3a86
TakaTaka 0 <tk.1987.6.9@gmail.com>
2017-07-30 17:19:45 +0900

    SORT NUMBERING

---
+++ sortNum.php
@@ -0,0 +1,16 @@
+<?php
+    while($input = trim(fgets(STDIN))){
+        $getNum[]= $input;
+               
+ }  
+    unset($getNum[0]);
+    $uniNum = array_unique($getNum);
+    sort($uniNum);
+                                        
+                                             
+                                             
+    foreach($uniNum as $key=>$sortNum){
+        echo $sortNum."\n";
+ }
+
+?>
